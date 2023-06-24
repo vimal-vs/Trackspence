37dc7c6ef11d13684ff1ba9d21347daad08f2dc9
Vimal Vs <vimalchan24@gmail.com>
2023-06-25 01:37:28 +0530

    name change

--- src/components/navBar/index.jsx
+++ src/components/navBar/index.jsx
@@ -6,7 +6,7 @@ const arima = Arima({ subsets: ['latin'], weight: '400' })
 export default function Navbar() {
   return (
     <div className='flex justify-between px-6 py-4 border-b '>
-      <a href='/' className={`text-3xl ${arima.className}`}>Expense Tracker</a>
+      <a href='/' className={`text-3xl ${arima.className}`}>TrackSpence</a>
       <div className='flex gap-4'>
         <ModeToggle />
         <button className="bg-blue-button px-2 py-1 rounded-sm cursor-not-allowed text-white">Sign in</button>
